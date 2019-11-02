12436806f8946ecef842a1ca5a7142e75fdb8277
JamsonWan <1940318814@qq.com>
2019-11-02 12:05:18 +0800

    insert success

---
+++ main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<time.h>
+#include "BPTree.h"
+
+int main(int argc, const char* argv[]){
+	int i;
+	BPTree T;
+	Result *res;
+
+	T = Initialize();
+
+	int data[] = {10, 15, 21, 37, 44, 51, 59, 63, 72, 85, 91, 97};
+	i = 1;
+	while(i < 13){
+		T = Insert(T, data[i-1], i);
+		i++;
+	}
+	Travel(T);
+	printf("\n");
+	TravelData(T);
+	printf("\n");
+	res = SearchBPTree(T,185);
+	if (res->tag == True){
+		printf("Find! value is: %d.\n", res->pt->value[res->i]);
+	}else{
+		printf("not found!\n");
+	}
+	Destroy(T);
+
+	return 0;
+}
